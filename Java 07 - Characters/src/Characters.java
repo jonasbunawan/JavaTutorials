a93f606c53eea11da88a6f5bb90c75569aca191b
Jonas Bunawan <jonas_bun@yahoo.com>
2016-09-06 12:22:17 +1000

    initial commit

---
+++ Java 07 - Characters/src/Characters.java
@@ -0,0 +1,16 @@
+
+public class Characters {
+	public static void main(String[]args){
+		char letter = 'a';
+		System.out.println(letter);
+		
+		char upperK = 75;//assigning value to char variable by specifying unicode of the letter
+		System.out.println(upperK);
+		
+		int K = 75;//assigning number to K
+		System.out.println((char)K);//interpret K variable to character (unicode to letter)
+		
+		char upK = 'K';
+		System.out.println((int)upK);//defining unicode of K
+	}
+}
